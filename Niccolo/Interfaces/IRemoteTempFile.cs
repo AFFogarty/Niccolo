985bfcaadd54ea15cf24ff103009cca43c76a556
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2020-07-10 20:49:25 -0700

    Interfaces for remote file systems with temporary files (#9)
    * Interfaces for remote file systems with temporary files
    
    * Comments

---
+++ Niccolo/Interfaces/IRemoteTempFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Niccolo.Interfaces
+{
+    /// <summary>
+    /// Represents a temporary file on a remote file system. Disposing this object deletes the 
+    /// file from the remote file system.
+    /// </summary>
+    interface IRemoteTempFile : IDisposable
+    {
+        /// <summary>
+        /// The path to the temporary file on the remote file system.
+        /// </summary>
+        string Path { get; }
+    }
+}
